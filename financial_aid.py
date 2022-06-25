9de8fcc2e0135ed03ccf7446332fd6420701e848
Hossam Elnemr <87553099+Hossam-Elnemr@users.noreply.github.com>
2022-06-25 05:30:58 +0200

    Another commit

---
+++ financial_aid.py
@@ -0,0 +1,48 @@
+'''
+Why should I do the financial aid application?
+- You can't submit your quizzes or view the assignments without purchasing th course or the financial aid,
+therefore, you will not be able to to take the certificate at the end of the course.
+So, if you want to submit the financial aid apllication, follow the following steps:
+
+1-  Using a computer, search for the course you want to apply financial aid for it.
+2-  Click on "Fiancial adi available"
+3-  Educational Background: Select "High school" if you're a high school student.
+4-  Annual Income = 0
+5-  Employment Status: Select "Student"
+
+6-  Fill out and submit your application with this text in the "Why are you applying for financial aid?"
+    "I’m a student from Egypt and want to learn Data Science. I’ve no job of my
+    own to carry the expenses to pay for the certificate of this course.
+    I live only for my scholarship, it is very much difficult for me to gather such amount of money for the certificate.
+    Financial Aid will help me take this course without any adverse impact on my monthly essential needs.
+    So I’m badly in need of this financial aid. I want to take this course as I want to learn.
+    This course will help a lot in my career and in university in the future. It will help perform better in understanding and learning this technology and give me an edge over my competitors.
+    A verified certificate will attach credibility to the certificate I receive from this course.
+    I plan to complete all assignments on or before time as I have done in previous Signature Track Courses. 
+    Also, I intend to participate in Discussion Forums, which I have found to supplement my learning immensely in the other online courses I have taken on Coursera.
+    I also plan to grade assignments that are to peer-reviewed which I believe will an invaluable learning opportunity."
+
+
+7-  Fill out and submit your application with this text in the"How will this course help you with your goals?"
+    My main career goal is to learn every day.
+    I really want to learn and to progress in my career.
+    Programming requires constant learning and improving.
+    Taking this course will help me to learn and study this Data Science and also to implement it.
+    It can help me advance in my knowledge.
+    This course will help me in defining Data Science, understanding how Python could potentially impact our business and industry,
+    to write a thought leadership piece regarding use cases and industry potential of Data Science, explain Data Science to clients, friends, joining a community of economists, business leaders, entrepreneurs,
+    and technologists that are shaping this technology as we speak.
+    Identifying which aspects of Data Science seem most important and relevant to us, Walking away with a strong foundation in where Data Science is going, what it does, and how to prepare for it.
+    Data Science course will help me achieve it to learn. Courses on Coursera helped me to greatly increase my programming knowledge in the past.
+  
+  
+  Finally, submit your application.
+  After you submit your application, it will take up to two weeks to review your application.
+  You can see course materials for free while you are waiting for your application to be approved by using the "audit mode".
+If your application is approved, you'll get an email letting you know, and you'll be automatically enrolled in the course. 
+Courses have personalized deadlines, so you won't need to worry about falling behind. 
+
+If you do not complete the course within 180 days, you may submit another application.
+I hope that this was helpful.
+Good luck!
+''' 
